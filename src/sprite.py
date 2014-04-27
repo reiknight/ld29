8554d837133f756ea2d0e2c28b58954cf19a34f6
ultrayoshi <david.morcillo@gmail.com>
2014-04-27 12:35:09 +0200

    Changed asset path

--- src/sprite.py
+++ src/sprite.py
@@ -4,7 +4,7 @@ import pygame
 class Sprite:
     """This class, which depends on pygame, it's basically an image for drawing"""
     def __init__(self, name):
-        self.path = "../assets/" + name
+        self.path = "assets/" + name
         self.image = pygame.image.load(self.path)
         
     def draw(self, surface, x, y):
