1ab1ef2606676d0fc1588085d22106a8f076f49c
Reik Val <guardian_de_las_sombras@hotmail.com>
2014-04-27 16:58:51 +0200

    Player now collides properly with the level

--- src/player.py
+++ src/player.py
@@ -6,21 +6,31 @@ from sprite import Sprite
 
 class Player(Entity):
     """Player entity"""
-    def __init__(self, level, x = 0, y = 0):
+    def __init__(self, level, x = 750, y = 0):
         self.x = x
         self.y = y
         self.w = CELL_SIZE
         self.h = CELL_SIZE
-        self.posX = x//CELL_SIZE
+        self.posX = (x + CELL_SIZE//2)//CELL_SIZE
         self.posY = y//CELL_SIZE
         self.level = level
         self.sprite = Sprite("player.png")
         self.mov = 0
     
     def update(self):
-        self.x += self.mov
-        if not self.level.cells[self.posY+1][self.posX].isSolid():
+        falling = False
+        if not self.level.cells[self.posY+1][self.posX + (0 if self.mov > 0 else 1)].isSolid():
             self.y += 5
+            falling = True
+        else:
+            falling = False
+        if self.mov < 0:
+            if not self.level.cells[self.posY + (1 if falling else 0)][(self.x + self.mov)//CELL_SIZE].isSolid():
+                self.x += self.mov
+        elif self.mov > 0:
+            if not self.level.cells[self.posY + (1 if falling else 0)][(self.x + self.mov)//CELL_SIZE+1].isSolid():
+                self.x += self.mov
+        
         self.posX = self.x//CELL_SIZE
         self.posY = self.y//CELL_SIZE
 
